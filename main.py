a69b3881bdf4bedf4871995c240da4f72b3f8c72
Mohammed Javidh S <120474876+javidh0@users.noreply.github.com>
2023-02-28 23:08:06 +0530

    upd2

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import math
 class PublicKeyCrypto:
     __power = None
     __div = None
-    def __init__(self, random_seed = 1, Range:list[int] = [0, 100]) -> None:
+    def __init__(self, random_seed = 1, Range:list[int] = [2, 10]) -> None:
         random.seed(random_seed)
         self.__power = random.randint(Range[0], Range[1])
         random.seed(random_seed+Range[1])
         self.__div = random.randint(Range[0], Range[1])
         print("Public Key Generation Succesfull")
     def __Compute(self, a):
-        return chr(math.pow(ord(a), self.__power) % self.__div)
+        a1 = math.pow(self.__power, ord(a))
+        a2 = int(math.remainder(a1, self.__div))
+        return str(a2)
     def Encypt(self, value:str):
-        return str(map(self.__Compute, tuple(value)))
+        print(self.__power, self.__div)
+        tr = ""
+        for i in value:
+            tr += self.__Compute(i)
+        return tr
