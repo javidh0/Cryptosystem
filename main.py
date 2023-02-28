fded4b5eb646d5d671d38f080366b7bfcea1f1f5
Mohammed Javidh S <120474876+javidh0@users.noreply.github.com>
2023-02-28 22:43:48 +0530

    a

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ import random
 class PublicKeyCrypto:
     __power = None
     __div = None
-    def __init__(self) -> None:
-        pass
+    def __init__(self, random_seed = 1, Range:list[int] = [0, 100]) -> None:
+        random.seed(random_seed)
+        self.__power = random.randint(Range[0], Range[1])
+        random.seed(random_seed+Range[1])
+        self.__div = random.randint(Range[0], Range[1])
+        print("Public Key Generation Succesfull")
